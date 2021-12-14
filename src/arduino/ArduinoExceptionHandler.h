c20e055a542bc17cdfbb7f17eed9bd8973eb4f1c
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2021-12-14 22:34:31 +0100

    PROGMEM added

--- src/arduino/ArduinoExceptionHandler.h
+++ src/arduino/ArduinoExceptionHandler.h
@@ -8,13 +8,16 @@
 
 #pragma once
 #include <Arduino.h>
+#include <avr/pgmspace.h>
 
 namespace tp {
+
+const char ARDUINO_ERROR_STR[] PROGMEM = "CAN ISO TP erorr : ";
 
 struct ArduinoExceptionHandler {
         template <typename T> void operator() (T const &error)
         {
-                Serial.print ("CAN ISO TP erorr : ");
+                Serial.print (ARDUINO_ERROR_STR);
                 Serial.println (int (error));
         }
 };
